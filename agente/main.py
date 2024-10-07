d92a4ac4136f8a1d34b0d4a6b4fe50a2172053f8
Steve <165537257+SteveCasTo@users.noreply.github.com>
2024-10-06 22:33:27 -0400

    Update main.py

--- agente/main.py
+++ agente/main.py
@@ -1,5 +1,6 @@
 
 from utiles.graficador import graficar_arbol
+from utiles.culminador import resultado
 from entorno import Entorno
 from limpiadorPetpal import LimpiadorPetpal
 from busquedas import Heuristica, Ciega
@@ -22,31 +23,32 @@ def main():
     print("Estado inicial del entorno:")
     entorno.imprimirEntorno(agente.posicion)
 
-    print("BUSQUEDA DE PRIMERO EN ANCHURA")
     # EJECUCION DE AMBAS BUSQUEDAS
+
+    print("BUSQUEDA DE PRIMERO EN ANCHURA")
+
     solucion, nodos_explorados, conexiones, niveles = Ciega.buscar(entorno, agente)  
-
-    if solucion is not None:
-        print("Imprimiendo los pasos:")
-        # Simula el proceso de moverse y limpiar
-        for paso in solucion:
-            agente.mover(paso)
-            agente.limpiar(entorno)  # Limpia las casillas adyacentes con basura
-            entorno.imprimirEntorno(agente.posicion)  # Imprime cada paso
-            complejidad = len(nodos_explorados)
-
-        print("--MEDIDAS DE RENDIMIENTO--")
-        print("Completitud: Si \nOptima: Si (dado el metodo de busqueda)")
-        print("Complejidad Temporal: {} u/t".format(complejidad))
-        print("Complejidad Espacial: {} u/e".format(complejidad))         
-    else:
-        print("No se encontró solución \n")
-        print("--MEDIDAS DE RENDIMIENTO--")
-        print("Completitud: NO \nOptima: - \n")
-        print("Complejidad Temporal: - \n")
-        print("Complejidad Espacial: -")
-
-
+    
+    #Devolver resultado del análisis de búsqueda no informada
+    resultado(agente, entorno, solucion, nodos_explorados)
+    
+    print("BUSQUEDA DE HEURISTICA VORAZ")
+    
+    entorno_h = Entorno(
+        ancho=10, 
+        largo=6, 
+        obstaculos=[(0,0), (2,0), (3,0), (4,0), 
+                    (0,2), (6,2), 
+                    (0,3), (2,3), (3,3), (4,3), (6,3), 
+                    (0,4),  
+                    (0,5), (1,5), (2,5), (3,5), (4,5), (5,5), (7,5), (8,5)], 
+        desechos=[(0, 1), (7, 2), (2, 4), (9,5)]
+    )
+    agente_h = LimpiadorPetpal(estacion_carga=(8, 0))
+    solucion_h, nodos_explorados_h, conexiones, niveles = Heuristica.buscar(entorno_h, agente_h)  
+    
+    #Devolver resultado del análisis de búsqueda informada
+    resultado(agente_h, entorno_h, solucion_h, nodos_explorados_h)
 
 if __name__ == "__main__":
     main()
