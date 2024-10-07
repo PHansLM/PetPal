84ac40750fa7d2a83365c5d10b9b4f00ab8978cd
Steve <165537257+SteveCasTo@users.noreply.github.com>
2024-10-06 21:59:30 -0400

    Update Heuristica.py

--- agente/busquedas/Heuristica.py
+++ agente/busquedas/Heuristica.py
@@ -1,10 +1,14 @@
 import heapq
+from utiles.graficador import graficar_arbol
 
 def buscar(entorno, PetPal):
     frontera = []
     heapq.heappush(frontera, (0, entorno.copy(), PetPal.posicion, []))
     visitados = set()
     paso = 0
+    nodos = []  # Almacenar nodos explorados
+    conexiones = []  # Almacenar conexiones (nodo_padre, nodo_hijo)
+    niveles = {}  # Diccionario para almacenar niveles de nodos
 
     print("Inicio de la búsqueda con heurística")
 
@@ -15,18 +19,30 @@ def buscar(entorno, PetPal):
 
         if estado_actual in visitados:
             continue
+        
         visitados.add(estado_actual)
+        niveles[posicion_actual] = paso
 
         if entorno_act.prueba_meta():
             print("¡Solución encontrada!\n")
-            return path + [posicion_actual]
+
+            solucion = path + [posicion_actual]  # Ruta Solución
+
+            solucion_pasos = [(posicion) for posicion in solucion]
+            print("Posiciones de la solución:")
+            for pos in solucion_pasos:
+                print(f"Posición: {pos}")
+
+            # Graficar el árbol con la solución
+            graficar_arbol(nodos, conexiones, niveles, solucion=solucion)
+
+            return path + [posicion_actual], nodos, conexiones, niveles
 
         PetPal.mover(posicion_actual)
         PetPal.limpiar(entorno_act)
 
         # Obtener sucesores
         sucesores = entorno_act.get_sucesores(posicion_actual)
-        print(f"Sucesores de {posicion_actual}: {sucesores}")
 
         # Si hay sucesores, encontramos el sucesor con menor heurística
         if sucesores:
@@ -39,15 +55,20 @@ def buscar(entorno, PetPal):
                 # Si no hay desechos restantes, la heurística es 0
                 mejor_sucesor = sucesores[0]  # Cualquier sucesor
                 nueva_h = 0
-                
+
             # Expandimos el sucesor con menor heurística
             nuevo_estado = (mejor_sucesor, frozenset(entorno_act.desechos))
             if nuevo_estado not in visitados and entorno_act.posicion_valida(mejor_sucesor):
                 heapq.heappush(frontera, (nueva_h, entorno_act.copy(), mejor_sucesor, path + [posicion_actual]))
+            conexiones.append((posicion_actual, mejor_sucesor))
+            for sucesor in sucesores:
+                if sucesor != mejor_sucesor:
+                    nodos.append(sucesor)
+                    conexiones.append((posicion_actual, sucesor))
         paso += 1
 
     print("No se encontró solución.")
-    return None  # Si no se encontró solución
+    return None, nodos, conexiones, niveles  # Si no se encontró solución
 
 def distancia_manhattan(a, b):
     """Calcula la distancia Manhattan entre dos puntos (x1, y1) y (x2, y2)."""
